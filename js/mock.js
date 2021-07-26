716da0c19b8fc09825e217535ca6bff5ecc70dbe
aantoshkin <antoshkin@glolime.com>
2021-07-26 09:26:13 +0300

    added save schema to JSONBin.io

--- js/mock.js
+++ js/mock.js
@@ -18,7 +18,7 @@ function mockEntityInputsMass () {
 	const mockInputRowData = 
 	[
 		[
-			{T_entityColumnDesc_T: 'Слова1'},
+			{T_entityColumnDesc_T: 'Слов222а1'},
 			{T_entityVarType_T: 'string'},
 			{T_entityColumnType_T: 'string'},
 			{T_entityColumnName_T: 'value'},
@@ -26,7 +26,7 @@ function mockEntityInputsMass () {
 			{T_entityVarName_T: 'value'}
 		],
 		[
-			{T_entityColumnDesc_T: 'Слова2'},
+			{T_entityColumnDesc_T: 'Слова2111'},
 			{T_entityVarType_T: 'string'},
 			{T_entityColumnType_T: 'string'},
 			{T_entityColumnName_T: 'value2'},
@@ -43,14 +43,107 @@ function mockEntityInputsMass () {
 			addEntityItemRow()
 	    }
 	})
-	const json = JSON.stringify(mockInputRowData);
-	console.log(json)
-	const dat = JSON.parse(json)
-	console.log(dat)
-	// url()
+	// const json = JSON.stringify(mockInputRowData);
+	// console.log(json)
+	// const dat = JSON.parse(json)
+	// console.log(dat)
+	// createNoteInCollectionApi(json)
 }
 
+const apiKey = '$2b$10$SU0HWnuoYolbM9jXGcV85uM6N5L/VOOavCZzeibovAJRp8EkySrSW'
+const collectionId = '60faf610a263d14a297ad80b'
 
+function getMainForm () {
+	return getInputsArr('entityMainDataFormInput') // G_mainForm
+}
+function getSchema () {
+	return getArrOfSameNamesDynamicInputs('entityItemsFormInput', 6) // G_itemsForm
+}
+
+function saveBinToCollectionApi(){
+
+	const json = {
+		// main:  JSON.stringify(getMainForm()),
+		// items: JSON.stringify(getSchema())
+		main: getMainForm(),
+		items: getSchema()
+	}
+	let req = new XMLHttpRequest();
+
+	req.onreadystatechange = () => {
+	  if (req.readyState == XMLHttpRequest.DONE) {
+	    console.log(req.responseText);
+	  }
+	};
+	  
+	req.open("POST", "https://api.jsonbin.io/v3/b", true);
+	req.setRequestHeader("Content-Type", "application/json");
+	req.setRequestHeader("X-Master-Key", apiKey);
+	req.setRequestHeader("X-Collection-Id", collectionId);
+	
+	req.send(JSON.stringify(json));
+}
+
+function createCollectionApi () {
+	let req = new XMLHttpRequest();
+
+	req.onreadystatechange = () => {
+	  if (req.readyState == XMLHttpRequest.DONE) {
+	    console.log(req.responseText);
+	  }
+	};
+
+	req.open("POST", "https://api.jsonbin.io/v3/c", true);
+	req.setRequestHeader("X-Collection-Name", "Doctrinizer");
+	req.setRequestHeader("X-Master-Key", apiKey);
+	req.send();
+}
+
+function getCollectionApi () {
+	let req = new XMLHttpRequest();
+
+	req.onreadystatechange = () => {
+	  if (req.readyState == XMLHttpRequest.DONE) {
+	    // console.log(req.responseText);
+	    res = JSON.parse(req.responseText)
+	    res.forEach(el => {
+	    	console.log(el)
+	    	getBin(el.record).then(binContent => {
+	    		console.log(binContent)
+	    	})
+	    })
+	  }
+	};
+
+	req.open("GET", "https://api.jsonbin.io/v3/c/" + collectionId + "/bins", true);
+	req.setRequestHeader("X-Master-Key", apiKey);
+	req.send();
+}
+
+// function getOneBin () {
+// 	let req = new XMLHttpRequest();
+
+// 	req.onreadystatechange = () => {
+// 	  if (req.readyState == XMLHttpRequest.DONE) {
+// 	    console.log(req.responseText);
+// 	  }
+// 	};
+
+// 	req.open("GET", "https://api.jsonbin.io/v3/b/<BIN_ID>/<BIN_VERSION | latest>", true);
+// 	req.setRequestHeader("X-Master-Key", "<YOUR_API_KEY>");
+// 	req.send();
+// }
+
+function getBin(binId) {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", "https://api.jsonbin.io/v3/b/" + binId);
+		xhr.setRequestHeader("X-Master-Key", apiKey);
+		xhr.onload = () => resolve(xhr.responseText);
+		xhr.onerror = () => reject(xhr.statusText);
+		xhr.send();
+	});
+}
 
 function mockMainForm () {
 	const mockMainFormData = [
@@ -63,3 +156,10 @@ function mockMainForm () {
 		input.setAttribute("value", Object.values(mockMainFormData[i])[0])
     }) 
 }
+// note
+// 60fade6e99892a4ae9a8c3df
+// collection
+//60faf610a263d14a297ad80b
+
+
+// https://jsonbin.io/api-reference/v3/bins/read
